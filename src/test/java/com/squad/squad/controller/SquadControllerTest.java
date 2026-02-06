f85d47cadb7630c3f1860ca5b9d504135bfa3e78
ijeee <jeng832@gmail.com>
2026-02-07 00:34:45 +0900

    Fix testa

--- src/test/java/com/squad/squad/controller/SquadControllerTest.java
+++ src/test/java/com/squad/squad/controller/SquadControllerTest.java
@@ -11,8 +11,8 @@ import com.squad.squad.service.SquadService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.bean.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -22,7 +22,7 @@ import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -35,7 +35,7 @@ class SquadControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private SquadService squadService;
 
     private static final LocalDateTime NOW = LocalDateTime.now();
